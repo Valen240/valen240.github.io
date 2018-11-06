980253e3f318d9678e0fc464891de82818a8a103
Erick <>
2018-11-05 22:07:09 -0600

    Add bootstrap js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log("Hello, and welcome to my portfolio site!");
-
 $(document).ready(function() {
   $(".project-preview").on("click", function() {
     $(this).siblings(".project-details").slideToggle();
